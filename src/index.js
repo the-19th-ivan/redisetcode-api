86a7ea7f6917518234efb759ca11805ad0772f11
Ivan Ramos <ivanandesramos.io@gmail.com>
2023-10-08 15:52:15 +0800

    setup

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const AppError = require('./utils/appError.util');
+const errorHandler = require('./utils/errorHandler.util');
+
+const app = express();
+
+/* MIDDLEWARE */
+
+app.use(helmet()); // Set security HTTP headers
+app.use(cors());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); // Development logging
+}
+
+app.use(express.json({ limit: '10kb' })); // Body parser, reading data from body into req.body
+
+/* ROUTES */
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(errorHandler);
+
+module.exports = app;
