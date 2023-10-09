f88a592b0dded92ff994e5232c403e4d0765734c
Ivan Ramos <ivanandesramos.io@gmail.com>
2023-10-09 09:55:11 +0800

    add protected routes to users

--- src/users/index.js
+++ src/users/index.js
@@ -3,16 +3,23 @@ const router = require('express').Router();
 const usersController = require('./users.controller');
 const { userValidation } = require('./users.middleware');
 const validator = require('../utils/validator.util');
+const { isAuth, restrictTo } = require('../auth/auth.middleware');
 
 router
   .route('/')
-  .get(usersController.getAll)
-  .post(userValidation, validator, usersController.create);
+  .get(isAuth, restrictTo('admin'), usersController.getAll)
+  .post(
+    isAuth,
+    restrictTo('admin'),
+    userValidation,
+    validator,
+    usersController.create,
+  );
 
 router
   .route('/:id')
   .get(usersController.getOne)
-  .patch(usersController.update)
-  .delete(usersController.delete);
+  .patch(isAuth, restrictTo('admin'), usersController.update)
+  .delete(isAuth, restrictTo('admin'), usersController.delete);
 
 module.exports = router;
